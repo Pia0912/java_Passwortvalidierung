aa34c20c55af591aab3393378fcf41123917f764
LorTho <134836725+LorTho@users.noreply.github.com>
2023-06-02 11:20:19 +0200

    initial branch Lorenz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,104 @@
 public class Main {
+    public static void main(String[] args) {
+        createPassword("test");
+    }
+    public static void createPassword(String password) {
+        boolean length = passwordLength(password);
+        boolean number = isThereNumber(password);
+        boolean small = smallChar(password);
+        boolean large = largeChar(password);
+        boolean notBad = badPassword(password);
+
+        //Analyse of Länge, Groß/Kleinschreibung, Zahlen
+        int count = 0;
+        String answer ="";
+        if (length) {
+            count++;
+            answer += "Länge: ok ";
+        }else
+            answer += "Länge: zu kurz! ";
+        if(number){
+            count++;
+            answer += "Nummern: ok ";
+        }else
+            answer += "Nummern: keine! ";
+        if(small){
+            count++;
+            answer += "Kleinbuchstaben: ok ";
+        }else
+            answer += "Kleinbuchstaben: keine! ";
+        if(large){
+            count++;
+            answer += "Großbuchstabe: ok ";
+        }else
+            answer += "Großbuchstabe: keine! ";
+
+        switch (count){
+            case 1:
+                System.out.println("passwort: schwach");
+                System.out.println(answer);
+                break;
+            case 2:
+                System.out.println("passwort: mittel");
+                System.out.println(answer);
+                break;
+            case 3:
+                System.out.println("passwort: gut");
+                System.out.println(answer);
+                break;
+            case 4:
+                System.out.println("passwort: sehr gut");
+                System.out.println(answer);
+                break;
+            default:
+                System.out.println("passwort: sehr schwach");
+                System.out.println(answer);
+                break;
+        }
+        if(!notBad)
+            answer = "Das Passwort ist schei**";
+    }
+
+    public static boolean badPassword(String password) {
+        String[] badPass = {"123456", "passwort"};
+        for (String pass : badPass) {
+            if (password.equals(pass))
+                return false;
+        }
+        return true;
+    }
+    public static boolean largeChar(String password) {
+        char[] charArray = password.toCharArray();
+        boolean large= false;
+        for (int i = 0; i < password.length(); i++) {
+            if(Character.isUpperCase(charArray[i]))
+                large = true;
+        }
+        return large;
+    }
+
+    public static boolean smallChar(String password) {
+        char[] charArray = password.toCharArray();
+        boolean small= false;
+        for (int i = 0; i < password.length(); i++) {
+            if(Character.isLowerCase(charArray[i]))
+                small = true;
+        }
+        return small;
+    }
+
+    public static boolean isThereNumber(String password) {
+        char[] charArray = password.toCharArray();
+        boolean number = false;
+        for (int i = 0; i < password.length(); i++) {
+            if(Character.isDigit(charArray[i]))
+                number = true;
+        }
+        return number;
+    }
+
+    public static boolean passwordLength(String length){
+        boolean ok = length.length() > 5;
+        return ok;
+    }
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class MainTest {
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "abc; false",
+                    "abcde; false",
+                    "abcdefg; true",
+                    "DG53gfds; true",
+            },
+            delimiter = ';'
+    )
+    void compare_length(String password, boolean expected){
+        Assertions.assertEquals(expected, Main.passwordLength(password));
+    }
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "abc1; true",
+                    "assde; false",
+                    "3e4rSD; true",
+            },
+            delimiter = ';'
+    )
+    void areThereNumbers(String password, boolean expected){
+        Assertions.assertEquals(expected, Main.isThereNumber(password));
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "abc1; true",
+                    "ADFGE3; false",
+                    "3e4rSD; true",
+            },
+            delimiter = ';'
+    )
+    void areThereSmallChars(String password, boolean expected){
+        Assertions.assertEquals(expected, Main.smallChar(password));
+    }
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "abc1; false",
+                    "ADFGE3; true",
+                    "3e4rSD; true",
+                    "fdgh456fdf5; false"
+            },
+            delimiter = ';'
+    )
+    void areThereLargeChars(String password, boolean expected){
+        Assertions.assertEquals(expected, Main.largeChar(password));
+    }
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "passwort; false",
+                    "ADFGE3; true",
+                    "3e4rSD; true",
+                    "123456; false",
+            },
+            delimiter = ';'
+    )
+    void badPassword(String password, boolean expected){
+        Assertions.assertEquals(expected, Main.badPassword(password));
+    }
+}
