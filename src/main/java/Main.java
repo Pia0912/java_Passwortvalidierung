8f349a99107f1a19c38ef6d6b2ef310e297fe2fc
Ben-21 <bs@schaefer-inet.de>
2023-06-02 11:45:02 +0200

    update methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,67 @@
 public class Main {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        //String Test = checkIfKnownWeakPasswordsIncluded("123");
     }
 
-    public static int checkStringLength(String word){
+    public static int checkStringLength(String word) {
         return word.length();
     }
 
-    public static boolean checkIfNumbersIncluded(String word){
+    public static boolean checkIfNumbersIncluded(String word) {
         return word.matches(".*[0-9].*");
     }
 
-    public static boolean checkIfCapitalLettersIncluded(String word){
-        if (word.matches(".*[A-Z].*")){
+    public static boolean checkIfCapitalLettersIncluded(String word) {
+        if (word.matches(".*[A-Z].*")) {
             return true;
-        }else {
+        } else {
             return false;
         }
+    }
+//==============================================
+    /*
+        public static String checkIfKnownWeakPasswordsIncluded(String word){
+            String[] weakPasswords = {"123", "1234", "123456", "12345678", "123456789", "qwerty"};
+            String result = "Check failed";
+           for (int i = 0; i < weakPasswords.length; i++) {
+               if (word.contains(weakPasswords[i])) {
+                   result = "Password not strong enough";
+                   break;
+               } else {
+                   result = "Password valid";
+               }
+           }
+           return result;
+        }
+     */
+//==============================================
+
+    public static String checkIfKnownWeakPasswordsIncluded(String word) {
+        String result = "Check failed";
+        switch (word) {
+            case "123":
+                result = "Password not strong enough";
+                break;
+            case "1234":
+                result = "Password not strong enough";
+                break;
+            case "123456":
+                result = "Password not strong enough";
+                break;
+            case "12345678":
+                result = "Password not strong enough";
+                break;
+            case "123456789":
+                result = "Password not strong enough";
+                break;
+            case "qwerty":
+                result = "Password not strong enough";
+                break;
+            default:
+                result = "Password valid";
+                break;
+        }
+        return result;
     }
 
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
 
     @Test
-    void yieldLength7_whenPuttingInAufgabe(){
+    void yieldLength7_whenPuttingInAufgabe() {
         String input = "Aufgabe";
 
         int actual = Main.checkStringLength(input);
@@ -13,8 +13,8 @@ public class MainTest {
         Assertions.assertEquals(actual, expected);
     }
 
-     @Test
-    void yieldtrue_whenPuttingInAufgabe1(){
+    @Test
+    void yieldtrue_whenPuttingInAufgabe1() {
         String input = "Aufgabe1";
 
         boolean actual = Main.checkIfNumbersIncluded(input);
@@ -22,8 +22,9 @@ public class MainTest {
 
         Assertions.assertEquals(actual, expected);
     }
+
     @Test
-    void yieldtfalse_whenPuttingInAufgabe(){
+    void yieldtfalse_whenPuttingInAufgabe() {
         String input = "Aufgabe";
 
         boolean actual = Main.checkIfNumbersIncluded(input);
@@ -33,11 +34,40 @@ public class MainTest {
     }
 
     @Test
-    void yieldtrue_whenPuttinginWelt(){
+    void yieldtrue_whenPuttinginWelt() {
         String input = "Welt";
 
         boolean actual = Main.checkIfCapitalLettersIncluded(input);
         boolean expected = true;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void yieldnotStrong_whenCheckingWeakPasswordqwerty() {
+        String input = "qwerty";
+
+        String actual = Main.checkIfKnownWeakPasswordsIncluded(input);
+        String expected = "Password not strong enough";
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    void yieldnotStrong_whenCheckingWeakPassword123() {
+        String input = "123";
+
+        String actual = Main.checkIfKnownWeakPasswordsIncluded(input);
+        String expected = "Password not strong enough";
+
+        Assertions.assertEquals(actual, expected);
+    }
+ @Test
+    void yieldnotStrong_whenCheckingWeakPassword12345678() {
+        String input = "12345678";
+
+        String actual = Main.checkIfKnownWeakPasswordsIncluded(input);
+        String expected = "Password not strong enough";
 
         Assertions.assertEquals(actual, expected);
     }
